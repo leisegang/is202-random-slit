bebabf7205bbb5390eadbee3d9603d693f23f5bf
kjetilhomme39@gmail.com <kjetilhomme39@gmail.com@629f3654-5bad-e6a6-240c-878465b0713a>
2013-09-10 08:54:18 +0000

    Starten på prosjektet

---
+++ 05-HRMS-v2/src/java/is202/hrms/entity/Department.java
@@ -0,0 +1,94 @@
+package is202.hrms.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+public class Department implements Serializable {
+
+    private static final long serialVersionUID = 42L;
+    @Id
+    @GeneratedValue
+    private long depNo;
+
+    // The name must not be null, and contain at least one character
+    @NotNull @Size(min=1)
+    private String name;
+
+    /** This should have been a list. java.util.Set is used here to
+     * prevent multiple registration of the same employee. In the
+     * final version the db will take care of that, so it will be
+     * safe to use List again. */
+    @OneToMany(mappedBy = "department")
+    private Set<Employee> employees;
+
+    public Department() {
+        employees = new HashSet<Employee>();
+    }
+
+    public long getDepNo() {
+        return depNo;
+    }
+
+    public void setDepNo(long depNo) {
+        this.depNo = depNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addEmployee(Employee emp) {
+        employees.add(emp);
+    }
+
+
+    public List<Employee> getEmployees() {
+        return new ArrayList<Employee>(employees);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (int) (this.depNo ^ (this.depNo >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Department other = (Department) obj;
+        if (depNo != other.depNo) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "" + depNo + " " + name;
+    }
+}
---
+++ 05-HRMS-v2/src/java/is202/hrms/entity/Employee.java
@@ -0,0 +1,84 @@
+package is202.hrms.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue
+    private long id;
+    
+    // Name is not null, and minimum one char
+    @NotNull @Size(min=1)
+    private String name;
+    
+    // salary cannot be negative
+    @Min(0)
+    private int salary;
+    
+    @ManyToOne
+    private Department department;
+
+    public Employee() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        department.addEmployee(this);
+        this.department = department;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Employee other = (Employee) obj;
+        if (id != other.id) return false;
+        return true;
+    }
+}
