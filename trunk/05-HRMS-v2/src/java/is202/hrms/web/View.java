8d63e4b6076738e111ea92e2e5201b5439b39be8
kjetilhomme39@gmail.com <kjetilhomme39@gmail.com@629f3654-5bad-e6a6-240c-878465b0713a>
2013-09-10 08:54:18 +0000

    Starten på prosjektet

---
+++ trunk/05-HRMS-v2/src/java/is202/hrms/web/View.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is202.hrms.web;
+
+/**
+ *
+ * @author evenal
+ */
+public enum View {
+    index,
+    departments,
+    department,
+    employees,
+    employee,
+    projects,
+    project,
+    prjmembers;
+}
---
+++ trunk/05-HRMS-v2/src/java/is202/hrms/web/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the classes which support the JSF based user interface.
+ * In addition to the backing beans there a couple of utility classes.
+ */
+package is202.hrms.web;
